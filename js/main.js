30780c0d6a7b420779740e71038466d2e8953de9
lucasrueda12 <lucasrueda64@gmail.com>
2022-07-07 01:29:52 -0300

    Aplicacion de css, removi footer y agregue 2 iconos

--- js/main.js
+++ js/main.js
@@ -1,25 +1,33 @@
-/* Algoritmo para calcular el maximo de una lista de numeros */
+let nombre = prompt("Ingresa tu nombre");
+let apellido = prompt("Ingresa tu apellido");
+let añoDeNacimiento = prompt("Ingresa tu año de nacimiento");
+let añoActual = 2022;
 
-console.log("Calcular el maximo de una lista");
+const edadActual = (añoActual, añoDeNacimiento) => añoActual - añoDeNacimiento;
 
-/* Defino las variables */
-console.log("");
-
-let array = [1 , 2 , 83 , 4 , 95 , 60 , 7 , 48 , 19 , 10];
-let aux = array[0];
-
-/* muestro la lista */
-
-console.log("Los elementos de la lista son: (" + array + ")");
-console.log("");
-
-/* funcion */
-
-for (let i = 0; i < array.length; i++) {
-    if (aux<array[i]){
-        aux = array[i];
+function permisoParaJugar(){
+    let edad = edadActual(añoActual, añoDeNacimiento);
+    if ( edad > 16){
+        console.log("Puede jugar este juego. Tienes " + edad );
+        return true;
+    }else{
+        console.log("No cumple la edad necesaria para jugar");
+        return false;
     }
 }
 
-/* muestro el valor mas grande de la lista */
-console.log("El valor mas alto de la lista es: " + aux);
+function startGame(nombre, apellido){
+    if(permisoParaJugar()){
+        console.log("Bienvenido " + nombre + " " + apellido);
+        let respuesta = prompt("Estas listo para la aventura?");
+        if(respuesta == "si" || respuesta == "SI" || respuesta == "Si" || respuesta == "sI"){
+            console.log("Esa es la actitud");
+        }else{
+            console.log("No te veo muy motivado");
+        }
+
+
+    }
+}
+
+startGame(nombre, apellido);
