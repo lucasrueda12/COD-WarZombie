0c0ef9306c7fa2924ab67fed363b1cb8a9754ab2
lucasrueda12 <lucasrueda64@gmail.com>
2022-07-08 12:35:42 -0300

    funciones: condicionales, ciclo e interaccion

--- js/main.js
+++ js/main.js
@@ -4,9 +4,9 @@ let añoDeNacimiento = prompt("Ingresa tu año de nacimiento");
 let añoActual = 2022;
 
 const edadActual = (añoActual, añoDeNacimiento) => añoActual - añoDeNacimiento;
+let edad = edadActual(añoActual, añoDeNacimiento);
 
 function permisoParaJugar(){
-    let edad = edadActual(añoActual, añoDeNacimiento);
     if ( edad > 16){
         console.log("Puede jugar este juego. Tienes " + edad );
         return true;
@@ -18,16 +18,31 @@ function permisoParaJugar(){
 
 function startGame(nombre, apellido){
     if(permisoParaJugar()){
+
         console.log("Bienvenido " + nombre + " " + apellido);
+        
         let respuesta = prompt("Estas listo para la aventura?");
+        
         if(respuesta == "si" || respuesta == "SI" || respuesta == "Si" || respuesta == "sI"){
             console.log("Esa es la actitud");
         }else{
             console.log("No te veo muy motivado");
         }
 
+        console.log("Tu edad fue un numero impar " + impar() + " veces");
 
     }
+}
+
+function impar(){
+    let contadorNumImpar = 0;
+    
+    for(let i=0 ; i <= edad ; i++){
+        if(i%2 != 0){
+            contadorNumImpar++;
+        }
+    }
+    return contadorNumImpar;
 }
 
 startGame(nombre, apellido);
